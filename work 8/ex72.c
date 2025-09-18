0bdc51ebecb75cd9c1393e2d931acff2aec2f744
muzammil <muzammil.khanmatil@gmail.com>
2025-09-18 05:24:40 +0500

    optimize code

--- work 8/ex72.c
+++ work 8/ex72.c
@@ -1,59 +1,44 @@
 #include <stdio.h>
 
 int main() {
-    int regionCount[] = {2}; // Example data: 2 regions scanned
+    int regionCount = 2; // Number of regions
+    int highElevationCount = 0;
+    int sum; 
 
-    // Example elevation grid (Region 1, 3x3)
-    int elevationGrid[3][3] = {
-        {10, 15, 20},
-        {12, 18, 22},
-        {14, 20, 25}
+    // 3D array: [region][row][col]
+    int regions[2][3][3] = {
+        {   // Region 1
+            {10, 15, 20},
+            {12, 18, 22},
+            {14, 20, 25}
+        },
+        {   // Region 2
+            {5, 8, 11},
+            {7, 10, 13},
+            {9, 12, 15}
+        }
     };
 
-    // Example elevation grid (Region 2, 3x3)
-    int elevationGrid2[3][3] = {
-        {5, 8, 11},
-        {7, 10, 13},
-        {9, 12, 15}
-    };
+    
+    for (int r = 0; r < regionCount; r++) {
+        sum = 0; 
 
-    int highElevationCount = 0; 
-    int sum1 = 0;               
-    int sum2 = 0;               
+        printf("Region %d:\n", r + 1);
+        for (int i = 0; i < 3; i++) {         
+            for (int j = 0; j < 3; j++) {     
+                printf("%d ", regions[r][i][j]);
 
-    // ---------------- Region 1 ----------------
-    printf("Region 1:\n");
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            printf("%d ", elevationGrid[i][j]);
-
-            sum1 += elevationGrid[i][j];
-
-            if (elevationGrid[i][j] > 1000) {
-                highElevationCount++;
+                sum += regions[r][i][j];
+                if (regions[r][i][j] > 1000) {
+                    highElevationCount++;
+                }
             }
+            printf("\n");
         }
-        printf("\n");
+        printf("Average of Region %d = %d\n\n", r + 1, sum / 9);
     }
-    printf("Average of Region 1 = %d\n\n", sum1 / 9);
 
-    // ---------------- Region 2 ----------------
-    printf("Region 2:\n");
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            printf("%d ", elevationGrid2[i][j]);
 
-            sum2 += elevationGrid2[i][j];
-
-            if (elevationGrid2[i][j] > 1000) {
-                highElevationCount++;
-            }
-        }
-        printf("\n");
-    }
-    printf("Average of Region 2 = %d\n\n", sum2 / 9);
-
-    // ---------------- Final Result ----------------
     printf("Number of locations with high elevations: %d\n", highElevationCount);
 
     return 0;
