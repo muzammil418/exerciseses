6fd1b5d3a379caf47c0adbd86a6ad33288103965
muzammil <muzammil.khanmatil@gmail.com>
2025-10-04 07:39:08 +0500

    ex 83 fix

--- work 9/exercise83.c
+++ work 9/exercise83.c
@@ -5,20 +5,30 @@
 //Write a C program that accepts three integers from the user and finds the second largest number among them.
 
 int main() {
-    int n1, n2, n3;
+     int n1, n2, n3;
+   int largest, second = -999999;
 
     printf("Enter three numbers: ");
     scanf("%d %d %d", &n1, &n2, &n3);
 
-    if ((n1 > n2 && n1 < n3) || (n1 > n3 && n1 < n2)) {
-        printf("Second largest is: %d\n", n1);
+    largest = n1;
+    if (n2 > largest) {
+        largest = n2;
     }
-    else if ((n2 > n1 && n2 < n3) || (n2 > n3 && n2 < n1)) {
-        printf("Second largest is: %d\n", n2);
+    if (n3 > largest) {
+        largest = n3;
     }
-    else {
-        printf("Second largest is: %d\n", n3);
+    if (n1 < largest && n1 > second){  
+        second = n1;
     }
+    if (n2 < largest && n2 > second){
+        second = n2;
+    }
+    if (n3 < largest && n3 > second) {
+        second = n3;
+    }
+    printf("Second largest is: %d\n", second);
+    
 
     return 0;
 }
