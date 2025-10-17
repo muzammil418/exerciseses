e682c20a895c40b0914e482604b25d83d3d4fd69
muzammil <muzammil.khanmatil@gmail.com>
2025-10-17 06:48:18 +0500

    add the pointer but the code is not working some how

--- work 11/exercise107.c
+++ work 11/exercise107.c
@@ -6,19 +6,19 @@ struct queue {
 	int rear;
 };
 
-void push(struct queue q1, int value){
-	if(q1.rear == 4){
+void push(struct queue *q1, int value){
+	if(*q1.rear == 4){
 		printf("queue is full\n");
         return;
     }
-	q1.arr[q1.rear] = value;
-	q1.rear++;
+	*q1.arr[q1.rear] = value;
+	*q1.rear++;
 }
 
 int main() {
 	struct queue q;
 	
-	push(q, 10);
+	push(&q, 10);
 	
 	printf("%d", q.arr[0]);
 	
