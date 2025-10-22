ae56d81ab91190a8d97258fefd9e37bcb84fc1f9
muzammil <muzammil.khanmatil@gmail.com>
2025-10-22 05:00:59 +0500

    modify test.c

--- test file/test.c
+++ test file/test.c
@@ -5,16 +5,39 @@ i want to count the lenght of string the lenght of string is 3
 // 974
 
 */
-int main()
-{
-	int i = 0;
-	int lenght = 0;
-	char mystring[99] = "The quick brown";
-	int count = 0;
+typedef char first;
+typedef int date;
+
+struct person {
+	first name[50];
+	date birth;
+};
+
+// void printMyArray(struct person *p1, int sz){
+	// for(int i=0; i<sz; i++){
+		// printf("%s\n", *(p1->name + i) );
+		// // printf("%d\n", *(p1->birth + i) );
+	// }
+// }
 	
-	for (int i = 0; mystring[i] != '\0'; i++){
-		count++;
+int main(){
+	struct person p1[5] = {
+		{"nuketro0p3r", 1995},
+		{"gamexpro_59820", 2009},
+		{"iqstrikeryt_88228", 2011},
+		{"chyt1k", 2013},
+		{"x_xvoidwalker", 2013},
+	};
+	
+	struct person *ptr = p1;
+	
+	//printMyArray(&p1, 5);
+	
+	
+	for(int i=0; i<5; i++){
+		printf("print name: %s\n", ptr[i].name);
+		printf("print date: %d\n", ptr[i].birth);
+		printf("print Address: %d\n", ptr + i);
+		
 	}
-	printf("%d\n", count);
-	return 0;
 }
